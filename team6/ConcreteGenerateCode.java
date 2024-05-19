e8405e142f7062fb9b5abb8fb5c5f026d02c4fcd
leopard0315 <127277307+leopard0315@users.noreply.github.com>
2024-05-19 23:56:09 +0900

    Create ConcreteGenerateCode.java

---
+++ team6/ConcreteGenerateCode.java
@@ -0,0 +1,18 @@
+/**
+ * 코드 생성을 위한 구체 클래스 인터페이스.
+ */
+public interface ConcreteGenerateCode {
+    String generate(String data);
+}
+
+/**
+ * OpenAI 어댑터 클래스.
+ */
+public class OpenAIAdapter implements ConcreteGenerateCode {
+    private OpenAI openAI;
+
+    @Override
+    public String generate(String data) {
+        return ""; // TODO: OpenAI와의 연동을 통해 코드 생성 로직 구현
+    }
+}
